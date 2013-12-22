de4cac03bb66a3ecfd282bf25c855f2f7c95375a
monakaz <yukaiwpc@gmail.com>
2013-12-22 23:50:14 +0900

    remove javadoc directory. added konashi.jar generater task

--- javadoc/assets/navtree_data.js
+++
@@ -1,8 +0,0 @@
-var NAVTREE_DATA =
-[ [ "com.uxxu.konashi.lib", "com/uxxu/konashi/lib/package-summary.html", [ [ "Interfaces", null, [ [ "BleDeviceSelectionDialog.OnBleDeviceSelectListener", "com/uxxu/konashi/lib/BleDeviceSelectionDialog.OnBleDeviceSelectListener.html", null, "v2" ] ]
-, "" ], [ "Classes", null, [ [ "BleDeviceListAdapter", "com/uxxu/konashi/lib/BleDeviceListAdapter.html", null, "v2" ], [ "BleDeviceSelectionDialog", "com/uxxu/konashi/lib/BleDeviceSelectionDialog.html", null, "v2" ], [ "Konashi", "com/uxxu/konashi/lib/Konashi.html", null, "v2" ], [ "KonashiEvent", "com/uxxu/konashi/lib/KonashiEvent.html", null, "v2" ], [ "KonashiManager", "com/uxxu/konashi/lib/KonashiManager.html", null, "v2" ], [ "KonashiNotifier", "com/uxxu/konashi/lib/KonashiNotifier.html", null, "v2" ], [ "KonashiObserver", "com/uxxu/konashi/lib/KonashiObserver.html", null, "v2" ], [ "KonashiUtils", "com/uxxu/konashi/lib/KonashiUtils.html", null, "v2" ] ]
-, "" ] ]
-, "v2" ] ]
-
-;
-
